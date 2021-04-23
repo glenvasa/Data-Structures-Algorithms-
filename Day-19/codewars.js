fc3f5b16dbc764fa194326922fc7556d7b7cc597
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-04-23 14:52:56 -0400

    Create codewars.js

---
+++ Day-19/codewars.js
@@ -0,0 +1,41 @@
+// Josephus Permutation
+
+// [1,2,3,4,5,6,7] - initial sequence
+// [1,2,4,5,6,7] => 3 is counted out and goes into the result [3]
+// [1,2,4,5,7] => 6 is counted out and goes into the result [3,6]
+// [1,4,5,7] => 2 is counted out and goes into the result [3,6,2]
+// [1,4,5] => 7 is counted out and goes into the result [3,6,2,7]
+// [1,4] => 5 is counted out and goes into the result [3,6,2,7,5]
+// [4] => 1 is counted out and goes into the result [3,6,2,7,5,1]
+// [] => 4 is counted out and goes into the result [3,6,2,7,5,1,4]
+
+// josephus([1,2,3,4,5,6,7],3)==[3,6,2,7,5,1,4]
+let result = [];
+function josephus(items, k) {
+  if (items.length === 1) {
+    result.push(items[0]);
+    return result;
+  }
+}
+
+// josephus([1,2,3,4,5,6,7],3)
+
+// FizzBuzz less code version
+
+const fizzbuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    let result = "";
+    if (i % 3 === 0) {
+      result += "fizz";
+    }
+    if (i % 5 === 0) {
+      result += "buzz";
+    }
+    if (i % 5 !== 0 && i % 3 !== 0) {
+      result = i;
+    }
+    console.log(result);
+  }
+};
+
+fizzbuzz(33);
